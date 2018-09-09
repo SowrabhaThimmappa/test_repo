b3360655982a91bd7faf3e1e6d810e29f0c97693
Sowrabha <sowrabha.thimmappa@slkgroup.com>
2018-09-09 16:20:06 +0530

    auto trigger

--- TEst/src/com/slk/test/Test.java
+++ TEst/src/com/slk/test/Test.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Second line");
 	System.out.println("another line");
 System.out.println("one more line");
+System.out.println("auto trigger build");
 }
 }
